e977aab1a7c971e066b57c7fc885e263d6e9c556
PirvanAndrei <pirvanandrei@ymail.com>
2015-09-04 10:25:49 +0200

    Working MVC

---
+++ WageCalculation/WageCalculation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WageCalculation.Startup))]
+namespace WageCalculation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

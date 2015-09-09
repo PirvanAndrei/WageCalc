a46562b28163c7fda36a6e2f6044a1bd2f3bab9a
chrlaura <chrlaura@gmail.com>
2015-09-09 16:05:31 +0200

    Added Qunit and simple test

--- WageCalculation/WageCalculation/Controllers/HomeController.cs
+++ WageCalculation/WageCalculation/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace WageCalculation.Controllers
             return View();
         }
 
+        public ActionResult Qunit()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ WageCalculation/WageCalculation/Scripts/maintests.js
@@ -0,0 +1,5 @@
+﻿QUnit.test("Simple test", function (assert) {
+    var num1 = 2;
+    var num2 = 3;
+    assert.equal(num1+num2, 5);
+});
